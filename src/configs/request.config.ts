39b14248ab0ec68d294beb5571cd8e741f6ee71f
ShinHyunJong <ian@therovers.net>
2026-01-11 17:18:31 +0900

    env

--- src/configs/request.config.ts
+++ src/configs/request.config.ts
@@ -1,3 +1,5 @@
-export const API_ENDPOINT =
-  'https://sample-server-1063234667970.asia-east2.run.app';
+import { apiUrl } from './env.config';
+
+export const API_ENDPOINT = apiUrl || 'http://localhost:3005';
+console.log('API_ENDPOINT:', API_ENDPOINT);
 // export const API_ENDPOINT = 'http://localhost:3005';
